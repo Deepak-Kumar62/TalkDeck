d85b5b6543f237e48d9075f4991e64e663008e91
Deepak_Kumar <ydeepak62076@gmail.com>
2024-10-27 16:55:25 +0530

    2nd part complete - ui design

---
+++ frontend/src/pages/signup/GenderCheckBox.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+const GenderCheckBox = () => {
+  return (
+    <div className="flex">
+      <div className="form-control">
+        <label htmlFor="" className={`label gap-2 cursor-pointer`}>
+          <span className="label-text ">Male</span>
+          <input
+            type="checkbox"
+            name=""
+            id=""
+            className="checkbox border-slate-900"
+          />
+        </label>
+      </div>
+
+      <div className="form-control">
+        <label htmlFor="" className={`label gap-2 cursor-pointer`}>
+          <span className="label-text ">Female</span>
+          <input
+            type="checkbox"
+            name=""
+            id=""
+            className="checkbox border-slate-900"
+          />
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default GenderCheckBox;
+
+
+
+
+
+
+
+// Starter code this file
+
+// import React from "react";
+
+// const GenderCheckBox = () => {
+//   return (
+//     <div className="flex">
+//       <div className="form-control">
+//         <label htmlFor="" className={`label gap-2 cursor-pointer`}>
+//           <span className="label-text ">Male</span>
+//           <input
+//             type="checkbox"
+//             name=""
+//             id=""
+//             className="checkbox border-slate-900"
+//           />
+//         </label>
+//       </div>
+
+//       <div className="form-control">
+//         <label htmlFor="" className={`label gap-2 cursor-pointer`}>
+//           <span className="label-text ">Female</span>
+//           <input
+//             type="checkbox"
+//             name=""
+//             id=""
+//             className="checkbox border-slate-900"
+//           />
+//         </label>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default GenderCheckBox;
+
